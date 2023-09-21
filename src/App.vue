cf68b8a07338346c01b3d01aab50cca7a65e58d9
user <user@WINDOWS10-BARE>
2023-09-21 11:04:36 +0800

    0921 v1

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
-  <!-- <HeaderBar isLogin="false" curUser="UUSSDSD"/> -->
+  <HeaderBar isLogin="false" curUser="UUSSDSD"/>
   <RouterView/>
 </template>
 
 <script setup>
 import {RouterView} from "vue-router";
-// import HeaderBar from './components/HeaderBar.vue';
+import HeaderBar from './components/HeaderBar.vue';
 </script>
 
 <style>
